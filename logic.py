5e1a9ff618c0193b5af59b6f9c4c9a1980d43ce1
KingAMR35 <am.ryabov@inbox.ru>
2025-06-25 16:24:28 +0400

    add some changes

--- logic.py
+++ logic.py
@@ -13,6 +13,12 @@ class Pokemon:
         self.name = self.get_name()
         self.index = self.game_index()
         self.abilities = self.game_abilities()
+        self.hp = randint(200, 400)
+        self.power = randint(30, 60)
+        self.level = 1
+        self.experience = 0
+        self.feed_count = 0
+        self.achievements = []
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -51,11 +57,47 @@ class Pokemon:
             return (data['abilities'][0]['ability'])
         else:
             return "Pikachu"
+    def feed(self):
+        self.feed_count += 1
+        self.gain_experience(10)
+        print(f"{self.name} съел еду!")
 
+    def gain_experience(self, amount):
+        self.experience += amount
+        while self.experience >= 100 * self.level:
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        self.experience -= 100 * (self.level - 1)
+        achievement_title = f"Уровень {self.level} достигнут"
+        achievement_description = f"{self.name} достиг нового уровня!"
+        new_achievement = Achievement(achievement_title, achievement_description)
+        self.achievements.append(new_achievement)
+        print(f"{self.name} теперь уровня {self.level}.")
+
+    def achievements_list(self):
+        return "\n".join([f"- {a.title}: {a.description}" for a in self.achievements])
+
+    def feed_info_1(self):
+        return (
+            f"Имя: {self.name}\n"
+            f"Уровень: {self.level}\n"
+            f"Опыт: {self.experience}/{100*self.level}\n"
+            f"Кормлений: {self.feed_count}\n"
+            f"Достижения:\n{self.achievements_list()}"
+        )
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покемона: {self.name}"
+        pokemon_data = (
+            f"Имя твоего покемона: {self.name}\n"
+            f"Сила покемона: {self.power}\n"
+            f"Здоровье покемона: {self.hp}\n"
+            f"Индекс твоего покемона: {self.index}\n"
+            f"Способность твоего покемона: {self.abilities}"
+        )
+        return pokemon_data
 
     # Метод класса для получения картинки покемона
     def show_img(self):
@@ -67,5 +109,37 @@ class Pokemon:
     def show_abilities(self):
         return f"Способность твоего покемона: {self.abilities}"
 
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): #если enemy - объект класса Wizard, то True, иначе False
+            chance = randint(1,5)
+            if chance == 3: 
+                return "Покемон волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f'Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}'
+        else:
+            enemy.hp = 0
+            return f'Покемон @{self.pokemon_trainer} победил в сражении @{enemy.pokemon_trainer}'
+class Wizard(Pokemon):
+    pass
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(2, 10)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f'\nБоец применил супер-атаку силой {super_power}'
+                
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
 
-
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
+class Achievement:
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
--- main.py
+++ main.py
@@ -11,10 +11,55 @@ def go(message):
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
+
+    else:
+        bot.reply_to(message, "Ты уже создал себе покемона.")
+
+
+
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
+
+@bot.message_handler(commands=['feed'])
+def feed(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        level = 1
+        feed = 1
+        feed += 1
+        bot.send_message(message.chat.id, "Вы покормили покемона")
+    else:
+        bot.send_message(message.chat.id, "Ты еще не создал себе покемона")
+        
+    if feed == 2:
+        level = 2
+        bot.send_message(message.chat.id, "Вы повысили уровень покемона")
+        
+        
+        
+@bot.message_handler(commands=['info'])
+def pokemons(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
         bot.send_message(message.chat.id, pokemon.game_index)
         bot.send_message(message.chat.id, pokemon.game_abilities)
     else:
-        bot.reply_to(message, "Ты уже создал себе покемона")
+        bot.send_message(message.chat.id, "Ты еще не создал себе покемона")
+        
+        
+        
+        
 
 
 bot.infinity_polling(none_stop=True)
